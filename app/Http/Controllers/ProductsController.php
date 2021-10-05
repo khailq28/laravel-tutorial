a91f7b497b792d82e744b566ed8ce4111cb5bac7
khailq28 <khailuong61@gmail.com>
2021-10-05 15:22:10 +0700

    #211005: passing data to the views (compact method, with method, directly in the view)

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,7 +7,24 @@ use Illuminate\Http\Request;
 class ProductsController extends Controller
 {
     public function index() {
-        return view('products.index');
+        $title = "Welcome to my laravel 8";
+        $description = "Created by Khai";
+        $data = [
+            'productOne' => 'iPhone',
+            'productTwo' => 'Samsung',
+        ];
+        
+        // compact method
+        // return view('products.index', compact('title', 'description'));
+
+        // with method
+        // return view('products.index')->with('data', $data);
+
+        // directly in the view
+        return view('products.index', [
+            'title' => $title,
+            'data' => $data
+        ]);
     }
 
     public function about() {
